6bb2565dc77e6f8243e32ba194e9c10b0d783907
Sung-won Kim <raptol@gmail.com>
2022-02-22 00:43:12 -0800

    add logic to display rating list by current location

--- app/src/main/java/com/example/friedchickenrating/fragments/ratings/RatingListFragment.java
+++ app/src/main/java/com/example/friedchickenrating/fragments/ratings/RatingListFragment.java
@@ -1,8 +1,18 @@
 package com.example.friedchickenrating.fragments.ratings;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
@@ -15,10 +25,22 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.example.friedchickenrating.R;
 import com.example.friedchickenrating.databinding.FragmentRatingListBinding;
+import com.firebase.geofire.GeoFireUtils;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQueryBounds;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -44,6 +66,10 @@ public class RatingListFragment extends Fragment implements RatingListAdapter.It
     private static final int SORT_OPTION_DESCENDING = 3;
     private static final int SORT_OPTION_LATEST = 4;
 
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private LatLng userLocation;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -70,7 +96,29 @@ public class RatingListFragment extends Fragment implements RatingListAdapter.It
         binding.recyclerViewRatingList.setLayoutManager(new GridLayoutManager(this.getContext(), 1));
         binding.recyclerViewRatingList.setAdapter(ratingListAdapter);
 
-        displayPlaceList();
+        //Check permission to get current location
+        if (!checkPermission()) {
+            requestPermission();
+        }
+
+        //Get current location
+        //Initiate LocationManager and LocationListener for current location of user
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                    Log.d(TAG, "onLocationChanged, latitude: " + latLng.latitude + ", longitude: " + latLng.longitude);
+            }
+        };
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        Location lastUserKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        userLocation = new LatLng(lastUserKnownLocation.getLatitude(), lastUserKnownLocation.getLongitude());
+
+        //display rating list
         displayRatingList(SORT_OPTION_LOCATION); //default sorting option: location
 
         binding.btnSortLocation.setOnClickListener(new View.OnClickListener() {
@@ -102,6 +150,28 @@ public class RatingListFragment extends Fragment implements RatingListAdapter.It
         });
     }
 
+    private boolean checkPermission() {
+        int hasFineLocationPermission =
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission =
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -117,7 +187,35 @@ public class RatingListFragment extends Fragment implements RatingListAdapter.It
                 .navigate(R.id.action_nav_ratings_to_nav_viewRatings);
     }
 
-    private void displayPlaceList() {
+    private void displayRatingList(int sortOption) {
+
+        Query query;
+
+        switch(sortOption) {
+            case SORT_OPTION_ASCENDING:
+                query = db.collection("ratings").orderBy("title", Query.Direction.ASCENDING);
+                readPlaceList();
+                readRatingList(query);
+                break;
+
+            case SORT_OPTION_DESCENDING:
+                query = db.collection("ratings").orderBy("title", Query.Direction.DESCENDING);
+                readPlaceList();
+                readRatingList(query);
+                break;
+
+            case SORT_OPTION_LATEST:
+                query = db.collection("ratings");
+                readPlaceList();
+                readRatingList(query);
+                break;
+
+            default: // by location
+                readRatingListByCurrentLocation(userLocation);
+        }
+    }
+
+    private void readPlaceList() {
         // Listen for realtime updates of the places
         db.collection("places")
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
@@ -142,27 +240,7 @@ public class RatingListFragment extends Fragment implements RatingListAdapter.It
                 });
     }
 
-    private void displayRatingList(int sortOption) {
-
-        Query query;
-
-        switch(sortOption) {
-            case SORT_OPTION_ASCENDING:
-                query = db.collection("ratings").orderBy("title", Query.Direction.ASCENDING);
-                break;
-
-            case SORT_OPTION_DESCENDING:
-                query = db.collection("ratings").orderBy("title", Query.Direction.DESCENDING);
-                break;
-
-            case SORT_OPTION_LATEST:
-                query = db.collection("ratings");
-                break;
-
-            default:
-                query = db.collection("ratings");
-        }
-
+    private void readRatingList(Query query) {
         // Listen for realtime updates of the ratings
         query.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
@@ -184,5 +262,65 @@ public class RatingListFragment extends Fragment implements RatingListAdapter.It
                 ratingListAdapter.setRatingList(ratingList, placeList);
             }
         });
+    }
+
+    private void readRatingListByCurrentLocation(LatLng currentLocation) {
+
+        placeList.clear();
+
+        //find places to be matched with user's current location
+        Log.d(TAG, "current location's latitude: " + currentLocation.latitude + ", longitude: " + currentLocation.longitude);
+
+        final GeoLocation center = new GeoLocation(currentLocation.latitude, currentLocation.longitude);
+        final double radiusInMeter = 5 * 1000; //find places within 5km
+
+        List<GeoQueryBounds> bounds = GeoFireUtils.getGeoHashQueryBounds(center, radiusInMeter);
+        final List<Task<QuerySnapshot>> tasks = new ArrayList<>();
+        for (GeoQueryBounds b : bounds) {
+            Query q = db.collection("places")
+                    .orderBy("geohash")
+                    .startAt(b.startHash)
+                    .endAt(b.endHash);
+
+            tasks.add(q.get());
+
+            Log.d(TAG, "tasks.added");
+        }
+
+        List<String> curLocPlaceIds = new ArrayList<>();
+
+        Tasks.whenAllComplete(tasks)
+                .addOnCompleteListener(new OnCompleteListener<List<Task<?>>>() {
+                    @Override
+                    public void onComplete(@NonNull Task<List<Task<?>>> t) {
+                        List<DocumentSnapshot> matchingPlaceDocs = new ArrayList<>();
+
+                        for (Task<QuerySnapshot> task : tasks) {
+                            QuerySnapshot snap = task.getResult();
+                            for (DocumentSnapshot doc : snap.getDocuments()) {
+                                double latitude = doc.getDouble("latitude");
+                                double longitude = doc.getDouble("longitude");
+
+                                // We have to filter out a few false positives due to GeoHash
+                                // accuracy, but most will match
+                                GeoLocation docLocation = new GeoLocation(latitude, longitude);
+                                double distanceInMeter = GeoFireUtils.getDistanceBetween(docLocation, center);
+                                if (distanceInMeter <= radiusInMeter) {
+                                    matchingPlaceDocs.add(doc);
+                                    com.example.friedchickenrating.fragments.ratings.Place place = doc.toObject(
+                                            com.example.friedchickenrating.fragments.ratings.Place.class);
+                                    placeList.add(place);
+                                    curLocPlaceIds.add(place.getPlaceid());
+                                    Log.d(TAG, "placeList: "+ place.getName());
+                                }
+                            }
+                        }
+
+                        // rating list matched with places
+
+                        Query query = db.collection("ratings").whereIn("placeid", curLocPlaceIds);
+                        readRatingList(query);
+                    }
+                });
     }
 }
