194fc265e345a322aa10811c8cb548a9b610d35f
Sung-won Kim <raptol@gmail.com>
2022-02-24 00:54:11 -0800

    add bottom sheet dialog fragment in Map link bottom sheet dialog with new rating

---
+++ app/src/main/java/com/example/friedchickenrating/Background.java
@@ -0,0 +1,30 @@
+package com.example.friedchickenrating;
+
+public class Background {
+    String id;
+    String name;
+
+    public Background() {
+    }
+
+    public Background(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
